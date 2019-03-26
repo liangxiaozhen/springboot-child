eb63b5d600da0542d2e9448623a69de54ba9856d
tom_glb <goodMorning_glb@atguigu.com>
2019-03-26 22:35:17 +0800

    CIO项目搭建

---
+++ walmart-web/walmart-admin/src/main/java/com/walmart/test/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.walmart.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class AdminApplication {
+
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+    
+}
